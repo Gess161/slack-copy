6dfb3794b978d0239ccc46caf034f536774a939d
kotulaigen <kotulai.gen@gmail.com>
2021-10-01 17:17:42 +0300

    private chat working

--- slack-copy/src/redux/reducers/userReducers/messagesSlice.js
+++ slack-copy/src/redux/reducers/userReducers/messagesSlice.js
@@ -8,11 +8,7 @@ const slice = createSlice({
     initialState,
     reducers: {
         messageReducer: (state, action) => {
-            if (Array.isArray(action.payload)) {
-                state.messages = [...state.messages, ...action.payload]
-            } else {
-                state.messages = [...state.messages, action.payload]
-            };
+            state.messages = [...state.messages, action.payload]
         },
         initialMessageReducer: (state, action) => {
             state.messages = [];
