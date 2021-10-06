fdcad2f43b52499f46b2b2afcbede47398390eeb
kotulaigen <kotulai.gen@gmail.com>
2021-10-06 15:43:11 +0300

    replaced plain jsx with components

--- slack-copy/src/components/Chat/index.js
+++ slack-copy/src/components/Chat/index.js
@@ -1,48 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { withRouter } from "react-router";
 import { useSelector, useDispatch } from "react-redux";
 import { socketReducer } from "../../redux/reducers/userReducers/userSlice";
 import { messageReducer } from "../../redux/reducers/userReducers/messagesSlice";
 import { fetchUser } from "../../redux/thunk/fetchUser";
-import ChatList from "./ChatList";
 import MessageContainer from "./MessageContainer";
-import RoomList from "./RoomList";
-import AddRoom from "./AddRoomBtn/index";
-import DeleteRoom from "./RemoveRoomBtn";
-import icon from "../../stylesheets/icons/bell.svg"
-import search from "../../stylesheets/icons/search.svg"
-import appIcon from "../../stylesheets/icons/app.svg"
-import usersNumber from "../../stylesheets/icons/chat-user.png"
+import { at, chatSearch, chatUser, info, overflow, phone, settings, star } from "../../stylesheets/icons/icons"
+import Sidebar from "./Sidebar";
+import MessageForm from "../MessageForm";
 
 function ChatRender(props) {
     const socket = props.socket;
     const dispatch = useDispatch();
-    const [message, setMessage] = useState('');
     const user = useSelector(state => state.user);
     const userStatus = useSelector(state => state.user.status);
-    const onKeyDownHandler = e => {
-        if (e.keyCode === 13) {
-            e.preventDefault();
-            sendData();
-        };
-    };
-    const sendData = () => {
-        if (message === '') return;
-        if (user.roomName === user.roomId) {
-            socket.emit('message', message, user.user, user.roomName, user.roomId);
-        } else {
-            socket.emit('private-message', {
-                senderName: user.user,
-                recipientName: user.roomName,
-                msg: message,
-                sender: user.socket,
-                recipient: user.roomId
-            })
-            const text = `${user.user}: ${message}`;
-            dispatch(messageReducer(text));
-        }
-        setMessage('');
-    };
+
     useEffect(() => {
         if (userStatus === 'idle') dispatch(fetchUser());
     }, [userStatus, dispatch]);
@@ -65,88 +37,40 @@ function ChatRender(props) {
     }, [userStatus, dispatch, user.roomName]);
 
     return (
-        <div className="chat">
-            <div className="chat-panel">
-                <h2 className="chat-panel-header ">
-                    <div className="header">
-                        <div className="header-top">
-                            <div className="header-text-name">Hlack Mockup</div>
-                            <div className="chat-panel-arrow"></div>
-                        </div>
-                        <div className="header-bottom">
-                            <div className="online-indicator"></div>
-                            <div className="header-bottom-user">{user.user}</div>
-                        </div>
-                    </div>
-                    <img src={icon} className="chat-panel-bell" />
-                </h2>
-                <div className="jump-to">
-                    <img className="search" src={search} />
-                    <input placeholder="Jump to..." />
-                </div>
-                <div className="apps">
-                    <img className="icon" src={appIcon} />
-                    <p>Apps</p>
-                </div>
-                {/* <AddRoom socket={socket} />
-                <DeleteRoom socket={socket} /> */}
-                <div className="channels">
-                    <p>Channels</p>
-                    <AddRoom socket={socket} />
-                </div>
-                <RoomList socket={socket} />
-                <ChatList me={user.user} socket={socket} />
-            </div>
-            <div className="message-container">
-                <div className="message-header">
-                    <div className="message-header-left">
-                        <div className="message-header-left-text">#{user.roomName}</div>
+        <div className="client">
+            <Sidebar user={user.user} socket={socket} />
+            <div className="chat-container">
+                <div className="chat-header">
+                    <div className="chat-header-left">
+                        <div className="chat-header-left-text">#{user.roomName}</div>
                         <div className="status">
-                            <img className="status-icon" src={usersNumber} />
-                            <div className="status-users">{3}</div>
+                            <img alt="icon" className="status-icon" src={chatUser} />
+                            <div className="status-users">{user.userList.length}</div>
                         </div>
                     </div>
-                    <div className="message-header-right">
+                    <div className="chat-header-right">
                         <div className="buttons-left">
-                            <img className="button-icon"/>
-                            <img className="button-icon"/>
-                            <img className="button-icon"/>
+                            <img src={phone} alt="icon" className="buttons-icon" />
+                            <img src={info} alt="icon" className="buttons-icon" />
+                            <img src={settings} alt="icon" className="buttons-icon" />
                         </div>
-                        <div className="message-search">
-                            <img className="button-icon"/>
-                            <input className="message-search-input" />
+                        <div className="chat-search">
+                            <img alt="icon" src={chatSearch} className="buttons-icon buttons-icon-search" />
+                            <input placeholder="Search" className="chat-search-input" />
                         </div>
                         <div className="buttons-right">
-                            <img className="button-icon"/>
-                            <img className="button-icon"/>
-                            <img className="button-icon"/>
+                            <img src={at} alt="icon" className="buttons-icon" />
+                            <img src={star} alt="icon" className="buttons-icon" />
+                            <img src={overflow} alt="icon" className="buttons-icon" />
                         </div>
                     </div>
                 </div>
                 <MessageContainer socket={socket} roomName={user.roomName} />
-                <form id="form" className="chat-form flex-column border border-dark">
-                    <div className="message-row d-flex flex-row">
-                        <label
-                            className="p-2"
-                            style={{ width: 100 }}
-                            htmlFor="message"> Message </label>
-                        <input
-                            onChange={(e) => setMessage(e.target.value)}
-                            onKeyDown={onKeyDownHandler}
-                            className="w-50 m-2"
-                            type="text"
-                            id="message"
-                            value={message} />
-                        <button
-                            onClick={sendData}
-                            className="w-50 m-2"
-                            type="button"
-                            id="send-button">Send</button>
-                    </div>
-                    <div className="d-flex p-2 flex-row">
-                        You logged in as: {user.user}
-                    </div>
-                </form>
+                <MessageForm
+                    user={user.user} 
+                    socket={socket}
+                    roomName={user.roomName} 
+                    roomId={user.roomId} />
             </div>
         </div>
     );
