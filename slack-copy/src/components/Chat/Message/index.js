de9e604cd7ddd4139b048ae93bf7c1bb43dbcd55
kotulaigen <kotulai.gen@gmail.com>
2021-10-11 14:04:17 +0300

    working timestamps

--- slack-copy/src/components/Chat/Message/index.js
+++ slack-copy/src/components/Chat/Message/index.js
@@ -1,13 +1,12 @@
 import { profilePicture } from "../../../stylesheets/icons/icons";
 
 const Message = ({message}) => {
-    console.log(message)
-    // const time = message.createdAt.slice(11, 16)
+    const time = message.time.slice(11, 16)
     return (
         <div className="message-container" >
             <img alt='userPicture' src={profilePicture} />
             <div className="message">
-                <h2 className="username">{message.senderName}<span>04:20</span></h2>
+                <h2 className="username">{message.senderName}<span>{time}</span></h2>
                 <p className="message-text" >{message.message}</p>
             </div>
         </div>
