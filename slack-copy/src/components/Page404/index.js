803bce2da8f8fc936a6fe4eac257db3b39a8016d
kotulaigen <kotulai.gen@gmail.com>
2021-10-12 14:28:44 +0300

    added page404

--- slack-copy/src/components/Page404/index.js
+++ slack-copy/src/components/Page404/index.js
@@ -1,8 +1,17 @@
-export default function NotFound() {
+import { withRouter } from "react-router";
+import notFound from "../../stylesheets/logo/404.png"
+
+function NotFound(props) {
+    const handleClick = () => {
+        props.history.push('/')
+    }
     return (
-        <div>
-            <h1>Oops</h1>
-            <h2>404 Not Found</h2>
+        <div className="not-found">
+            <img alt="404"src={notFound}/>
+            <h2>Page not found</h2>
+            <button onClick={handleClick} className="buttons-return">Return to Home</button>
         </div>
     );
 };
+
+export default withRouter(NotFound)
