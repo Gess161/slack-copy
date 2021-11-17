6850a65a6984ee3de770b180632feff5f6583552
Gess161 <newerr161@gmail.com>
2021-11-17 17:05:47 +0200

    fixed counter bug

--- slack-copy/src/components/Chat/Sidebar/index.js
+++ slack-copy/src/components/Chat/Sidebar/index.js
@@ -5,7 +5,7 @@ import ChatList from "./ChatList/index";
 import { app, bell, search } from "../../../stylesheets/icons/icons"
 
 const Sidebar = (props) => {
-    const { setAddRoomName, user, handleUserClick, addRoom, addRoomActive, usersList , handleAddRoomClick, handleRoomClick, rooms  } = props;
+    const { setAddRoomName, user, handleUserClick, addRoom, addRoomActive, usersList, handleAddRoomClick, handleRoomClick, rooms, unreadMessages } = props;
     const socket = user.socket
     const currentChat = user.roomName
     return (
@@ -33,10 +33,29 @@ const Sidebar = (props) => {
             </div>
             <div className="channels-rooms channels" >
                 <p>Channels</p>
-                <AddRoom active={addRoomActive} setAddRoomName={setAddRoomName} socket={socket} addRoom={addRoom} handleAddRoomClick={handleAddRoomClick} />
+                <AddRoom 
+                    active={addRoomActive} 
+                    setAddRoomName={setAddRoomName} 
+                    socket={socket} 
+                    addRoom={addRoom} 
+                    handleAddRoomClick={handleAddRoomClick} 
+                />
             </div>
-            <RoomList handleRoomClick={handleRoomClick} rooms={rooms} socket={socket} activeChat={currentChat} />
-            <ChatList handleRoomClick={handleUserClick} usersList={usersList } me={user} activeChat={currentChat} socket={socket} />
+            <RoomList 
+                unreadMessages={unreadMessages} 
+                handleRoomClick={handleRoomClick} 
+                rooms={rooms} 
+                socket={socket} 
+                activeChat={currentChat} 
+            />
+            <ChatList
+                unreadMessages={unreadMessages}
+                handleRoomClick={handleUserClick}
+                usersList={usersList}
+                me={user}
+                activeChat={currentChat}
+                socket={socket} 
+            />
         </div>
     )
 }
